6eb054a19dfcc29d507ad5e7aeee7b9c4f29bc83
Sai Rashwant <sairashwantno1@gmail.com>
2025-05-19 23:14:12 -0700

    Final Push

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,20 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 export default function CourseNavigation() {
+  const location = useLocation();
+  const path = location.pathname;
+
+  const links = [
+    { to: "/Kambaz/Courses/1234/Home", id: "wd-course-home-link", label: "Home" },
+    { to: "/Kambaz/Courses/1234/Modules", id: "wd-course-modules-link", label: "Modules" },
+    { to: "/Kambaz/Courses/1234/Piazza", id: "wd-course-piazza-link", label: "Piazza" },
+    { to: "/Kambaz/Courses/1234/Zoom", id: "wd-course-zoom-link", label: "Zoom" },
+    { to: "/Kambaz/Courses/1234/Assignments", id: "wd-course-quizzes-link", label: "Assignments" },
+    { to: "/Kambaz/Courses/1234/Quizzes", id: "wd-course-assignments-link", label: "Quizzes" },
+    { to: "/Kambaz/Courses/1234/People", id: "wd-course-people-link", label: "People" },
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kambaz/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kambaz/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
+      {links.map(({ to, id, label }) => {
+        const isActive = path === to;
+        const className = `list-group-item border border-0 ${
+          isActive ? "active" : "text-danger"
+        }`;
+
+        return (
+          <Link key={to} to={to} id={id} className={className}>
+            {label}
+          </Link>
+        );
+      })}
     </div>
-);}
+  );
+}
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,29 +1,62 @@
 import { AiOutlineDashboard } from "react-icons/ai";
-import { LiaBookSolid } from "react-icons/lia";
-import { FaComputer, FaRegCircleUser } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-import { ListGroup } from "react-bootstrap";
-import { CiCalendar, CiInboxIn } from "react-icons/ci";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Link, useLocation } from "react-router-dom";
+
 export default function KambazNavigation() {
+  const location = useLocation();
+
+  const navItems = [
+    { label: "Account", icon: FaRegCircleUser, path: "/Kambaz/Account" },
+    { label: "Dashboard", icon: AiOutlineDashboard, path: "/Kambaz/Dashboard" },
+    { label: "Courses", icon: LiaBookSolid, path: "/Kambaz/Courses" },
+    { label: "Calendar", icon: IoCalendarOutline, path: "/Kambaz/Calendar" },
+    { label: "Inbox", icon: FaInbox, path: "/Kambaz/Inbox" },
+    { label: "Labs", icon: LiaCogSolid, path: "/labs" },
+  ];
+
   return (
-    <ListGroup id="wd-kambaz-navigation" style={{ width: 120 }} 
-       className=" position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-    <ListGroup.Item id="wd-neu-link" target="_blank" action
-       href="https://www.northeastern.edu/"
-       className="bg-black border-0 text-center">
-       <img src="/images/Neu.png" width="75px" /></ListGroup.Item>
-      <ListGroup.Item to="/Kambaz/Account" as={Link} className="text-center border-0 bg-black text-white">
-      <FaRegCircleUser className="fs-1 text text-white" /><br /> Account 
+    <ListGroup
+      id="wd-kambaz-navigation"
+      style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 start-0 d-none d-md-block bg-black z-2"
+    >
+      <ListGroup.Item
+        id="wd-neu-link"
+        target="_blank"
+        action
+        href="https://www.northeastern.edu/"
+        className="bg-black border-0 text-center py-3"
+      >
+        <img src="/images/Neu.png" width="75px" alt="Northeastern Logo" />
       </ListGroup.Item>
-      <ListGroup.Item to="/Kambaz/Dashboard" as={Link} className="text-center border-0 bg-white text-danger">
-      <AiOutlineDashboard className="fs-1 text-danger" /><br />Dashboard 
-      </ListGroup.Item>
-      <ListGroup.Item to="/Kambaz/Dashboard" as={Link} className="text-white bg-black text-center border-0">
-      <LiaBookSolid className="fs-1 text-danger" /> Courses 
-      </ListGroup.Item>
-      <ListGroup.Item to="/Kambaz/Calendar" as={Link} className="text-white bg-black text-center border-0">
-      <CiCalendar className="fs-1 text-danger"/>Calendar</ListGroup.Item>
-      <ListGroup.Item to="/Kambaz/Inbox" as={Link} className="text-white bg-black text-center border-0"><CiInboxIn className="fs-1 text-danger"/><br/>Inbox</ListGroup.Item>
-      <ListGroup.Item to="/Labs" as={Link} className="text-white bg-black text-center border-0"><FaComputer className="fs-1 text-danger"/><br/>Labs</ListGroup.Item>
-      </ListGroup>
-);}
+
+      {navItems.map(({ label, icon: Icon, path }) => {
+        const isActive = location.pathname.startsWith(path);
+        const isAccount = label === "Account";
+
+        const backgroundColor = isActive ? "bg-white" : "bg-black";
+        const textColor = isActive ? "text-danger" : "text-white";
+        const iconColor = isAccount
+          ? isActive
+            ? "text-black"
+            : "text-white"
+          : "text-danger";
+
+        return (
+          <ListGroup.Item
+            key={label}
+            to={path}
+            as={Link}
+            className={`text-center border-0 py-3 ${backgroundColor} ${textColor}`}
+          >
+            <Icon className={`fs-1 ${iconColor}`} />
+            <div className="small">{label}</div>
+          </ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
+}
