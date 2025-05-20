30b4e59cc5170a96e4ae1f0f8372a171d73646f0
Sai Rashwant <sairashwantno1@gmail.com>
2025-05-19 23:30:51 -0700

    final pro max

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -16,7 +16,17 @@ export default function AssignmentEditor() {
             id="wd-description"
             className="w-50"
             rows={3}
-            defaultValue="The assignment is available online Submit a link to the landing page of"
+            defaultValue="The assignment is available online. Submit a link to the landing page of your Web application running on Netlify.
+
+            The landing page should be the Kambaz application with a link to the Lab exercises.
+
+            Lab 1 should be the landing page of the Lab exercises and should include the following:
+
+            Your full name and section
+            Links to each of the lab assignments
+            Link to the Kambaz application
+            Links to all relevant source code repositories
+            The Kambaz application should include a link to navigate back to the landing page."
           />
         </Form.Group>
 
