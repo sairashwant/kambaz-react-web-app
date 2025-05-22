98c0744fe5d683454ef9f892f7b934faf6ce66f9
Sai Rashwant <sairashwantno1@gmail.com>
2025-05-22 11:30:08 -0700

    A3- Completed till Highlight

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,20 +1,32 @@
+import Add from './Add';
 import AddingAndRemovingToFromArrays from './AddingAndRemovingToFromArrays';
 import ArrayIndexAndLength from './ArrayIndexandLength';
 import ArrowFunctions from './ArrowFunctions';
 import BooleanVariables from './BooleanVariables';
+import Classes from './Classes';
 import ConditionalOutputIfElse from './ConditionalOutputIfElse';
 import ConditionalOutputInline from './ConditionalOutputInLine';
+import Destructing from './Destructing';
+import DestructingImports from './DestructingImports';
 import FilterFunction from './FilterFunction';
 import FindFunction from './FindFunction';
 import FindIndex from './FindIndex';
 import ForLoops from './ForLoops';
+import FunctionDestructing from './FunctionDestructing';
+import House from './House';
 import IfElse from './IfElse';
 import ImpliedReturn from './ImpliedReturn';
+import JsonStringify from './JsonStringify';
 import LegacyFunctions from './LegacyFunctions';
 import MapFunction from './MapFunction';
 import SimpleArrays from './SimpleArrays';
+import Spreading from './Spread';
+import Square from './Square';
+import Styles from './Styles';
 import TemplateLiterals from './TemplateLiterals';
 import TernaryOperator from './TernaryOperator';
+import TodoItem from './TodoItem';
+import TodoList from './TodoList';
 import VariablesAndConstants from './VariablesAndConstants';
 import VariableTypes from './VariableTypes';
 
@@ -41,6 +53,20 @@ export default function Lab3() {
       <FindFunction />
       <FindIndex />
       <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
     </div>
   );
 }
