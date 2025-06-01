659a2b98455e1aa18c8b683ddc3f889cd8b3d861
Sai Rashwant <sairashwantno1@gmail.com>
2025-06-01 14:03:42 -0700

    First Commit

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+import ClickEvent from "./ClickEvent";
+
+export default function Lab4(){
+    return (
+        <Container>
+        <h1>Lab 4</h1>
+        <ClickEvent />
+        </Container>
+    )
+}
