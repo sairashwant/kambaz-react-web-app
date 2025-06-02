fa87950bf1520d03d0cc5b8ad972330b269338a4
Sai Rashwant <sairashwantno1@gmail.com>
2025-06-01 17:33:56 -0700

    Half done

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,11 +1,24 @@
 import { Container } from "react-bootstrap";
 import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
 
 export default function Lab4(){
+      function sayHello() {
+        alert("Hello");}
+
     return (
         <Container>
         <h1>Lab 4</h1>
         <ClickEvent />
+        <PassingDataOnEvent/>
+        <PassingFunctions theFunction={sayHello}/>
+        <EventObject/>
+        <Counter/>
+        <BooleanStateVariables/>        
         </Container>
     )
 }
