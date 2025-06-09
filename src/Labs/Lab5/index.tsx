11b47d1a1752d60a1212887cd9d0201dde39b030
Sai Rashwant <sairashwantno1@gmail.com>
2025-06-09 17:28:10 -0400

    Lab Completed

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,8 +1,12 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import Module from "./Module";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -18,5 +22,9 @@ export default function Lab5() {
       <QueryParameters />
       <WorkingWithObjects/>
       <Module />
+      <WorkingWithArrays/>
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously/>
+      <WorkingWithArraysAsynchronously />
     </div>
 );}
