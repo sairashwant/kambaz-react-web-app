8083465359846c7985fe1797586ca813583ede8c
Sai Rashwant <sairashwantno1@gmail.com>
2025-06-09 20:50:46 -0400

    Finished till sign up form

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -4,23 +4,64 @@ import * as client from "./client";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 import { FormControl } from "react-bootstrap";
+
 export default function Signup() {
   const [user, setUser] = useState<any>({});
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const signup = async () => {
     const currentUser = await client.signup(user);
     dispatch(setCurrentUser(currentUser));
     navigate("/Kambaz/Account/Profile");
   };
+
   return (
     <div className="wd-signup-screen">
       <h1>Sign up</h1>
-      <FormControl value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}
-             className="wd-username b-2" placeholder="username" />
-      <FormControl value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}
-             className="wd-password mb-2" placeholder="password" type="password"/>
-      <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100"> Sign up </button><br />
+
+      <FormControl className="mb-2" placeholder="Username"
+        value={user.username || ""}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <FormControl className="mb-2" type="password" placeholder="Password"
+        value={user.password || ""}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <FormControl className="mb-2" placeholder="First Name"
+        value={user.firstName || ""}
+        onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+      />
+      <FormControl className="mb-2" placeholder="Last Name"
+        value={user.lastName || ""}
+        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+      />
+      <FormControl className="mb-2" type="email" placeholder="Email"
+        value={user.email || ""}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <FormControl className="mb-2" type="date" placeholder="Date of Birth"
+        value={user.dob || ""}
+        onChange={(e) => setUser({ ...user, dob: e.target.value })}
+      />
+      <FormControl className="mb-2" placeholder="Role (e.g., STUDENT, FACULTY, ADMIN)"
+        value={user.role || ""}
+        onChange={(e) => setUser({ ...user, role: e.target.value })}
+      />
+      <FormControl className="mb-2" placeholder="Login ID"
+        value={user.loginId || ""}
+        onChange={(e) => setUser({ ...user, loginId: e.target.value })}
+      />
+      <FormControl className="mb-2" placeholder="Section"
+        value={user.section || ""}
+        onChange={(e) => setUser({ ...user, section: e.target.value })}
+      />
+
+      <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100">
+        Sign up
+      </button><br />
+
       <Link to="/Kambaz/Account/Signin" className="wd-signin-link">Sign in</Link>
     </div>
-);}
+  );
+}
